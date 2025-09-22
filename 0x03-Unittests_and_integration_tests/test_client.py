a2a728c82b56875b3da74deea52d8d22209e35db
Vohiro <dvohiro@gmail.com>
2025-09-22 13:36:52 +0100

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,7 +30,7 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_url = "https://api.github.com/orgs/test-org/repos"
         payload = {"repos_url": expected_url}
 
-        with patch.object(GithubOrgClient, "org", 
+        with patch.object(GithubOrgClient, "org",
                           new_callable=property(lambda self: payload)):
             client = GithubOrgClient("test-org")
             self.assertEqual(client._public_repos_url, expected_url)
@@ -41,9 +41,9 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_repos = [{"name": "repo1"}, {"name": "repo2"}]
         mock_get_json.return_value = expected_repos
 
-        with patch.object(GithubOrgClient, "_public_repos_url", 
-                          new_callable=PropertyMock) as mock_url:
-            mock_url.return_value="https://api.github.com/orgs/test-org/repos"
+        with patch.object(GithubOrgClient, "_public_repos_url",
+                          new_callable=PropertyMock) as mo_url:
+            mo_url.return_value = "https://api.github.com/orgs/test-org/repos"
             client = GithubOrgClient("test-org")
             result = client.public_repos()
 
@@ -51,7 +51,7 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, ["repo1", "repo2"])
 
             # Verify mocks were called exactly once
-            mock_url.assert_called_once()
+            mo_url.assert_called_once()
             mock_get_json.assert_called_once_with
             ("https://api.github.com/orgs/test-org/repos")
 
