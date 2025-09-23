28a3d518095652a7004915d6996d72d522c8f627
Vohiro <dvohiro@gmail.com>
2025-09-23 11:02:23 +0100

    api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,8 +4,12 @@ from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
-router.register(r"messages", MessageViewSet, basename="message")
+
+# Nested router for messages inside conversations
+conversations_router = routers.NestedDefaultRouter(router, r"conversations", lookup="conversation")
+conversations_router.register(r"messages", MessageViewSet, basename="conversation-messages")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(conversations_router.urls)),
 ]
